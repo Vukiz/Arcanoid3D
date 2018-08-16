548a8fffb917cf034e5c291e2ace8c069627770b
Alekseev Eugene <Vukiz1236@gmail.com>
2018-08-16 15:40:04 +0800

    added border coloring

---
+++ Arcanoid3D/Assets/Scripts/BorderLightUp.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class BorderLightUp : MonoBehaviour
+{
+  private Renderer borderRenderer;
+
+	void Start ()
+	{
+	  borderRenderer = GetComponent<Renderer>();
+	}
+	
+  void OnCollisionEnter(Collision col)
+  {
+    StartCoroutine(FadeColor(Color.red, Color.white));
+  }
+
+  private IEnumerator FadeColor(Color fromColor, Color toColor)
+  {
+    float duration = 0.5f;
+    float currentTime = 0f;
+    borderRenderer.material.color = fromColor;
+    while (currentTime < duration)
+    {
+      borderRenderer.material.color = Color.Lerp(fromColor, toColor, currentTime / duration);
+      currentTime += Time.deltaTime;
+      yield return null;
+    }
+  }
+}
